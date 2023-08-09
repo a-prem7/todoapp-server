ab7c2f9c698f3762676456491a17cd6c719dda19
amal-prem <aldamal.prem@gmail.com>
2023-08-09 15:54:41 -0400

    adds new connection

--- db.js
+++ db.js
@@ -1,12 +1,8 @@
-const Pool = require("pg").Pool;
-require("dotenv").config();
-
+const { Pool } = require("pg");
 const pool = new Pool({
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
   connectionString: process.env.DBConfigLink,
-  port: process.env.DBPORT,
-  database: "todoapp",
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
 module.exports = pool;
